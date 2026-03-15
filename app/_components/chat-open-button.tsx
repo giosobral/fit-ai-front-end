98c37f36ce3791b76c5887642f6b6c152c29b21e
Jhowman037 <giovanesobral10@hotmail.com>
2026-03-14 21:41:40 -0300

    feat: add chai ai and onboarding page

--- app/_components/chat-open-button.tsx
+++ app/_components/chat-open-button.tsx
@@ -1,16 +1,16 @@
 "use client";
 
 import { Sparkles } from "lucide-react";
-// import { useQueryStates, parseAsBoolean, parseAsString } from "nuqs";
+ import { useQueryStates, parseAsBoolean, parseAsString } from "nuqs";
 
 export function ChatOpenButton() {
-  // const [, setChatParams] = useQueryStates({
-  //   chat_open: parseAsBoolean.withDefault(false),
-  //   chat_initial_message: parseAsString,
-  // });
+  const [, setChatParams] = useQueryStates({
+    chat_open: parseAsBoolean.withDefault(false),
+    chat_initial_message: parseAsString,
+  });
 
   return (
-    <button className="rounded-full bg-primary p-4">
+    <button className="rounded-full bg-primary p-4" onClick={()=> setChatParams({chat_open: true})} >
       <Sparkles className="size-6 text-primary-foreground" />
     </button>
   );
